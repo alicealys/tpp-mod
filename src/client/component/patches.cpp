4074ddcd493ed94d76abb30f3d99b9bddfabf91a
alice <58637860+alicealys@users.noreply.github.com>
2025-08-25 03:17:27 +0200

    maybe better patch

--- src/client/component/patches.cpp
+++ src/client/component/patches.cpp
@@ -5,12 +5,18 @@
 
 #include <utils/hook.hpp>
 #include <utils/flags.hpp>
+#include <utils/nt.hpp>
 
 namespace patches
 {
 	namespace
 	{
-
+		void set_timer_resolution()
+		{
+			ULONG data{};
+			const utils::nt::library ntdll("ntdll.dll");
+			ntdll.invoke_pascal<void>("NtSetTimerResolution", 5000, TRUE, &data);
+		}
 	}
 
 	class component final : public component_interface
@@ -23,7 +29,9 @@ namespace patches
 				return;
 			}
 
-			utils::hook::nop(SELECT_VALUE(0x142F2AD7F, 0x14241449F), 0x5); // remove weird sleep
+			set_timer_resolution();
+
+			utils::hook::call(SELECT_VALUE(0x142F2AD7F, 0x14241449F), std::this_thread::yield); // remove weird sleep
 			utils::hook::jump(SELECT_VALUE(0x14008130A, 0x14008195A), SELECT_VALUE(0x1400814D8, 0x140081B28)); // unlock framerate always
 		}
 	};
