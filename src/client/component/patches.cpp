fa2a26b567f50f71c8705a7fb191370308b05171
alice <58637860+alicealys@users.noreply.github.com>
2025-08-25 17:56:41 +0200

    limit number of threads to 4

--- src/client/component/patches.cpp
+++ src/client/component/patches.cpp
@@ -17,6 +17,16 @@ namespace patches
 			const utils::nt::library ntdll("ntdll.dll");
 			ntdll.invoke_pascal<void>("NtSetTimerResolution", 5000, TRUE, &data);
 		}
+
+		void thread_sleep()
+		{
+			std::this_thread::yield();
+		}
+
+		unsigned int get_processor_count_stub()
+		{
+			return 4;
+		}
 	}
 
 	class component final : public component_interface
@@ -31,7 +41,9 @@ namespace patches
 
 			set_timer_resolution();
 
-			utils::hook::call(SELECT_VALUE(0x142F2AD7F, 0x14241449F), std::this_thread::yield); // remove weird sleep
+			utils::hook::call(SELECT_VALUE(0x142F2AD7F, 0x14241449F), thread_sleep);
+			utils::hook::jump(SELECT_VALUE(0x142F308E0, 0x14C6D6A20), get_processor_count_stub);
+
 			utils::hook::jump(SELECT_VALUE(0x14008130A, 0x14008195A), SELECT_VALUE(0x1400814D8, 0x140081B28)); // unlock framerate always
 		}
 	};
