4da64e81ea478ab706126012ea2b2f753f4d4c59
alice <58637860+alicealys@users.noreply.github.com>
2025-08-25 23:58:00 +0200

    make mgo launchable directly

--- src/client/component/patches.cpp
+++ src/client/component/patches.cpp
@@ -27,6 +27,24 @@ namespace patches
 		{
 			return 4;
 		}
+
+		void sub_143AA8300_stub(__int64 unk, unsigned int* res, int arg_count, const char** args)
+		{
+			unsigned int data{};
+			const char value[] = "99c85cdbf2c837d50d37c82af2c837d5c12d5e80fbc837d5f2c837d5f2c837d5f2";
+			utils::hook::invoke<unsigned int*>(0x1402DCD30, &data, unk + 288, value, sizeof(value) - 1);
+			*res = 0;
+		}
+
+		void unlock_fps()
+		{
+			set_timer_resolution();
+
+			utils::hook::call(SELECT_VALUE(0x142F2AD7F, 0x14241449F), thread_sleep);
+			utils::hook::jump(SELECT_VALUE(0x142F308E0, 0x14C6D6A20), get_processor_count_stub);
+
+			utils::hook::jump(SELECT_VALUE(0x14008130A, 0x14008195A), SELECT_VALUE(0x1400814D8, 0x140081B28)); // unlock framerate always
+		}
 	}
 
 	class component final : public component_interface
@@ -34,17 +52,16 @@ namespace patches
 	public:
 		void post_unpack() override
 		{
-			if (!utils::flags::has_flag("unlock-fps"))
+			if (game::environment::is_mgo())
 			{
-				return;
+				// /AppData 99c85cdbf2c837d50d37c82af2c837d5c12d5e80fbc837d5f2c837d5f2c837d5f2 (command line arg)
+				utils::hook::jump(0x143AA8300, sub_143AA8300_stub);
 			}
 
-			set_timer_resolution();
-
-			utils::hook::call(SELECT_VALUE(0x142F2AD7F, 0x14241449F), thread_sleep);
-			utils::hook::jump(SELECT_VALUE(0x142F308E0, 0x14C6D6A20), get_processor_count_stub);
-
-			utils::hook::jump(SELECT_VALUE(0x14008130A, 0x14008195A), SELECT_VALUE(0x1400814D8, 0x140081B28)); // unlock framerate always
+			if (utils::flags::has_flag("unlock-fps"))
+			{
+				unlock_fps();
+			}
 		}
 	};
 }
