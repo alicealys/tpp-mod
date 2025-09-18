806fb3ca9a9e28b739ee0d22d1bb03215110b581
alice <58637860+alicealys@users.noreply.github.com>
2025-09-18 23:51:03 +0200

    disable game window for server

--- src/client/component/dedicated_server.cpp
+++ src/client/component/dedicated_server.cpp
@@ -196,6 +196,31 @@ namespace dedicated_server
 			request_match_rotate = false;
 			request_disconnect = false;
 		}
+
+		__int64 gn_execute_stub()
+		{
+			return -1;
+		}
+
+		utils::hook::detour translate_messages_hook;
+
+		void translate_messages_stub(void* a1)
+		{
+			utils::hook::invoke<void>(0x140071410, a1);
+
+			while (true)
+			{
+				const auto ptr = *reinterpret_cast<void**>(0x141F28320);
+				if (ptr != nullptr)
+				{
+					utils::hook::invoke<void>(ptr, 0, 33281, 0, 0);
+					utils::hook::invoke<void>(ptr, 0, 33281, 0, 0);
+					break;
+				}
+
+				std::this_thread::sleep_for(1ms);
+			}
+		}
 	}
 
 	class component final : public component_interface
@@ -309,6 +334,11 @@ namespace dedicated_server
 			utils::hook::set<std::uint8_t>(0x146579190, 0xC3);
 			utils::hook::set<std::uint8_t>(0x14A91F2C0, 0xC3);
 			utils::hook::set<std::uint8_t>(0x140E1A4B0, 0xC3);
+
+			utils::hook::jump(0x141451F40, gn_execute_stub);
+			utils::hook::nop(0x14258DC10, 5);
+			translate_messages_hook.create(0x142590640, translate_messages_stub);
+
 		}
 	};
 }
