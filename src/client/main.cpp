404b6a0f7655c1718360749f36cc577d090698ba
alice <58637860+alicealys@users.noreply.github.com>
2025-12-09 19:46:21 +0100

    add version check

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,6 +4,8 @@
 #include "game/game.hpp"
 
 #include <utils/nt.hpp>
+#include <utils/io.hpp>
+#include <utils/cryptography.hpp>
 
 namespace
 {
@@ -25,9 +27,36 @@ namespace
 		const utils::nt::library user32{"user32.dll"};
 		user32.invoke<BOOL>("SetProcessDpiAwarenessContext", DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 	}
+	
+	bool check_version()
+	{
+		utils::nt::library self;
+		const auto path = self.get_path();
+		std::string data;
+		OutputDebugString(path.data());
+		if (!utils::io::read_file(path, &data))
+		{
+			return false;
+		}
+
+		const auto hash = utils::cryptography::sha1::compute(data, true);
+		if (hash != "17AC94A4BC9F88B035A45122C8A67EFA38D03F2A" /*tpp*/ &&
+			hash != "8690C7C27C94DD6A452BA6A612B7B485918F3BAF" /*mgo*/)
+		{
+			return false;
+		}
+
+		return true;
+	}
 
 	int start()
 	{
+		if (!check_version())
+		{
+			MessageBoxA(nullptr, "This version of TPP or MGO is not supported", "TPP-Mod: ERROR", MB_ICONERROR);
+			std::exit(0);
+		}
+
 		enable_dpi_awareness();
 
 		component_loader::pre_load();
