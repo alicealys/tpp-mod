7df6656eccf43fe3d6e4bcf2eec545af1b3eac30
WujekFoliarz <igorpodstawek14@gmail.com>
2026-05-06 16:55:59 +0200

    add hidapi in dualib.lua

--- deps/premake/duaLib.lua
+++ deps/premake/duaLib.lua
@@ -1,36 +1,67 @@
+hidapi = {
+    source = path.join(dependencies.basePath, "duaLib/src/thirdparty/hidapi")
+}
+
+function hidapi.import()
+    links { "hidapi" }
+    hidapi.includes()
+end
+
+function hidapi.includes()
+    includedirs { path.join(hidapi.source, "hidapi") }
+end
+
+function hidapi.project()
+    project "hidapi"
+        language "C"
+        kind "StaticLib"
+        
+        files {
+            path.join(hidapi.source, "hidapi/*.h"),
+            path.join(hidapi.source, "windows/hid.c"),
+        }
+        
+        warnings "Off"
+        
+        filter "system:windows"
+            defines { "HIDAPI_NO_HIDIO" }
+        filter {}
+end
+
+table.insert(dependencies, hidapi)
+
 duaLib = {
-	source = path.join(dependencies.basePath, "duaLib"),
+    source = path.join(dependencies.basePath, "duaLib"),
 }
 
 function duaLib.import()
-	links { "duaLib" }
-	duaLib.includes()
+    links { "duaLib" }
+    duaLib.includes()
 end
 
 function duaLib.includes()
-	includedirs {
-		path.join(duaLib.source, "src/include"),
-		path.join(duaLib.source, "src/thirdparty/hidapi/hidapi")
-	}
+    includedirs {
+        path.join(duaLib.source, "src/include"),
+    }
 end
 
 function duaLib.project()
-	project "duaLib"
-		language "C++"
-
-		duaLib.includes()
-
-		files {
-			path.join(duaLib.source, "src/include/**.h"),
-			path.join(duaLib.source, "src/source/**.c"),
-			path.join(duaLib.source, "src/include/**.hpp"),
-			path.join(duaLib.source, "src/source/**.cpp"),
-		}
-
-		warnings "Off"
-		kind "StaticLib"
-
-		defines { "DUALIB_EXPORTS" }
+    project "duaLib"
+        language "C++"
+        kind "StaticLib"
+        
+        duaLib.includes()
+        hidapi.import()
+        
+        files {
+            path.join(duaLib.source, "src/include/**.h"),
+            path.join(duaLib.source, "src/include/**.hpp"),
+            path.join(duaLib.source, "src/source/**.c"),
+            path.join(duaLib.source, "src/source/**.cpp"),
+        }
+        
+        warnings "Off"
+        defines { "DUALIB_EXPORTS" }
 end
 
 table.insert(dependencies, duaLib)
