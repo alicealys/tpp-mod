31d78a62578018a8f03fb255f33bdb73e83904d9
WujekFoliarz <igorpodstawek14@gmail.com>
2026-05-06 17:45:58 +0200

    open scepad handle

--- src/client/component/scepad.cpp
+++ src/client/component/scepad.cpp
@@ -8,6 +8,8 @@ namespace scepad
 {
     namespace
     {
+        static int padHandle = 0;
+
         void update_scepad()
         {
             console::info("[scepad] test from update_scepad");
@@ -18,14 +20,23 @@ namespace scepad
         public:
             void pre_load() override
             {
-                int init_res = scePadInit();
-                if(init_res == SCE_OK){
-                    console::info("[scepad] initialized successfully");
-                }
+
             }
 
             void start() override
             {
+                int init_res = scePadInit();
+                if(init_res == SCE_OK)
+                {
+                    console::info("[scepad] initialized successfully");
+                    padHandle = scePadOpen(1, 0, 0);
+                    console::info("[scepad] opened handle %d", padHandle);
+                }
+                else
+                {
+                    console::error("[scepad] failed to initialize duaLib");
+                }
+
                 scheduler::loop(update_scepad, scheduler::pipeline::main, 1s);
             }
         };
