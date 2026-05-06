0e9e65cdbc656d931c5ea164ce39612f0048e3be
WujekFoliarz <igorpodstawek14@gmail.com>
2026-05-06 18:45:17 +0200

    change get_weapon_type

--- src/client/component/scepad.cpp
+++ src/client/component/scepad.cpp
@@ -30,7 +30,12 @@ namespace scepad
             if (!is_player_initialized()) return 0;
 
             const auto player = game::tpp::gm::player::player2System->player2System;
+            if (!player) return 0;
+            if (!player->tpp.controller) return 0;
+            if (!player->tpp.controller->__vftable) return 0;
+
             void* weaponPtr = player->tpp.controller->__vftable->GetCurrentWeapon(player->tpp.controller, player->tpp.localPlayerIndex);
+            if(!weaponPtr) return 0;
 
             console::info("[scepad] GetCurrentWeapon returned: %p", weaponPtr);
 
