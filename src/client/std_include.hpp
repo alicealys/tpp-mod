f02784075df9996620b99dd1e5908a00eef901b7
alice <58637860+alicealys@users.noreply.github.com>
2026-09-04 21:29:01 +0200

    remove imgui dep

--- src/client/std_include.hpp
+++ src/client/std_include.hpp
@@ -100,11 +100,6 @@
 #include <asmjit/x86/x86assembler.h>
 
 #include <d3d11.h>
-#include <imgui.h>
-#include <imgui_internal.h>
-#include <backends/imgui_impl_dx11.h>
-#include <backends/imgui_impl_win32.h>
-#include <misc/cpp/imgui_stdlib.h>
 
 #pragma warning(pop)
 #pragma warning(disable: 4100)
