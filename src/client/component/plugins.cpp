2be54ec80b0a56e6ee5d2eb024a7b68f1e57530f
alice <58637860+alicealys@users.noreply.github.com>
2026-09-21 22:51:05 +0200

    add fs_load_plugins dvar

--- src/client/component/plugins.cpp
+++ src/client/component/plugins.cpp
@@ -3,13 +3,27 @@
 
 #include "filesystem.hpp"
 #include "console.hpp"
+#include "vars.hpp"
 
 #include <utils/hook.hpp>
 #include <utils/io.hpp>
 #include <utils/cryptography.hpp>
+#include <utils/flags.hpp>
 
 namespace plugins
 {
+	namespace
+	{
+		struct plugin_handle_t
+		{
+			std::string name;
+			HMODULE module;
+		};
+
+		std::vector<plugin_handle_t> loaded_plugins;
+		vars::var_ptr fs_load_plugins;
+	}
+
 	void delete_old_plugins()
 	{
 		const auto files = utils::io::list_files(".");
@@ -39,7 +53,7 @@ namespace plugins
 			}
 
 			const auto name = file.substr(file.find_last_of("\\/") + 1);
-			console::info("Loading plugin \"%s\"...\n", name.data());
+			console::info("[plugins] Loading plugin \"%s\"...\n", name.data());
 
 			const auto data = utils::io::read_file(file);
 			const auto hash = utils::cryptography::md5::compute(data, true);
@@ -49,7 +63,7 @@ namespace plugins
 			utils::io::write_file(new_name, data);
 			utils::io::hide_file(new_name);
 
-			utils::nt::library::load(new_name);
+			loaded_plugins.emplace_back(name, utils::nt::library::load(new_name));
 		}
 	}
 
@@ -58,16 +72,34 @@ namespace plugins
 	public:
 		void pre_load() override
 		{
+			fs_load_plugins = vars::register_bool("fs_load_plugins", true, vars::var_flag_saved, "enable plugin loading");
+
 			delete_old_plugins();
 		}
 
 		void start() override
 		{
+			if (!fs_load_plugins->current.enabled())
+			{
+				return;
+			}
+
 			const auto paths = filesystem::get_search_paths();
 			for (const auto& path : paths)
 			{
 				load_plugins(path + "/plugins");
 			}
+		}
+
+		void end() override
+		{
+			for (const auto& plugin : loaded_plugins)
+			{
+				console::info("[plugins] Unloading plugin \"%s\"...\n", plugin.name.data());
+				FreeLibrary(plugin.module);
+			}
+
+			loaded_plugins.clear();
 		}
 	};
 }
